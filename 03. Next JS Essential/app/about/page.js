7b5b0599be4d5cf9b8d4a214a2e1666c2ecc4d51
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-04-16 12:36:12 +0530

    Added nav links

---
+++ 03. Next JS Essential/app/about/page.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div className="bg-blue-950">About Page</div>;
+};
+
+export default About;
--- 03. Next JS Essential/app/page.js
+++ 03. Next JS Essential/app/page.js
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+    <main className="bg-gray-900 h-screen w-screen text-white flex flex-col items-center justify-center">
+      <img
+        className="w-20"
+        src="/logo.png"
+        alt="A server surrounded by magic sparkles."
+      />
+      <h1 className="text-3xl font-extrabold">
+        Welcome to this NextJS Course!
+      </h1>
+      <p className="text-xl">🔥 Let&apos;s get started! 🔥</p>
+      <Link href="/about">About</Link>
     </main>
   );
 }
