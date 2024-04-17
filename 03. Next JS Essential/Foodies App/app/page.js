f7b7b5225782fc157bf77173b46519d6d0325c29
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-04-17 13:36:12 +0530

    Tailwind css setup created

--- 03. Next JS Essential/Foodies App/app/page.js
+++ 03. Next JS Essential/Foodies App/app/page.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
+      <h1 className="text-white text-3xl font-bold">Time to get started!</h1>
     </main>
   );
 }
