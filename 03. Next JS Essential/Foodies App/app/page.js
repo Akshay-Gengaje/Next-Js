c0c553367218402e89ff6e1106635381b6abebd3
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-04-20 10:30:14 +0530

    Improved the css of home page

--- 03. Next JS Essential/Foodies App/app/page.js
+++ 03. Next JS Essential/Foodies App/app/page.js
@@ -4,56 +4,50 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header className="flex mt-20">
-        <div className="w-[30rem] ml-20">
+      <header className="flex items-center justify-between gap-2 px-4 py-8 mx-auto max-w-7xl">
+        <div className="w-96">
           <ImageSlideshow />
         </div>
-        <div className="flex-1">
-          <div>
-            <h1 className=" uppercase tracking-wide bg-gradient-to-tr from-orange-600 to-orange-400 bg-clip-text text-transparent text-3xl font-bold">
-              NextLevel Food For <br /> NextLevel Foodies
-            </h1>
-            <p className="mt-4 text-lg">
-              Taste & share food from all over the world.
-            </p>
-          </div>
-          <div className="mt-5 flex gap-6 items-center">
-            <Link href="/community" className=" text-orange-400">
-              Join The Community
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-3xl font-bold text-white uppercase tracking-wide">
+            NextLevel Food for NextLevel Foodies
+          </h1>
+          <p className="text-xl text-gray-300">
+            Taste & share food from all over the world.
+          </p>
+          <div className="flex space-x-4">
+            <Link
+              href="/community"
+              className="px-4 py-2 text-center text-white font-bold rounded-md bg-gradient-to-r from-orange-500 to-yellow-300 hover:bg-gradient-to-r hover:from-orange-600 hover:to-yellow-400"
+            >
+              Join the Community
             </Link>
             <Link
               href="/meals"
-              className="px-2 py-1 bg-gradient-to-r from-orange-600 to-orange-400 rounded-md font-bold"
+              className="px-4 py-2 text-center text-gray-300 font-bold rounded-md border border-gray-300 hover:text-white hover:border-white"
             >
               Explore Meals
             </Link>
           </div>
         </div>
       </header>
-      <main>
-        <section>
-          <h2 className="">How it works</h2>
-          <p>
+      <main className="px-4 py-8 mx-auto max-w-7xl">
+        <section className="mb-8 space-y-4">
+          <h2 className="text-2xl font-bold  text-center">How it works</h2>
+          <p className="text-xl ">
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes,
-            and to connect with other food lovers.
-          </p>
-          <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
+            recipes with the world. It's a place to discover new dishes, and to
+            connect with other food lovers.
           </p>
         </section>
-
-        <section>
-          <h2>Why NextLevel Food?</h2>
-          <p>
+        <section className="mb-8 space-y-4">
+          <h2 className="text-2xl font-bold  text-center">
+            Why NextLevel Food?
+          </h2>
+          <p className="text-xl ">
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes,
-            and to connect with other food lovers.
-          </p>
-          <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
+            recipes with the world. It's a place to discover new dishes, and to
+            connect with other food lovers.
           </p>
         </section>
       </main>
